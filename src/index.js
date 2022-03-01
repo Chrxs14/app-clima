9f1b72cf5cbed929bfd616d2179a900412f64114
Chrxs14 <trabajo.ochoachristopher@gmail.com>
2022-02-28 23:44:15 -0500

    mejora en el sistema de busqueda

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ async function fetchCity(){
     let cityName = inputCity.value
     cityName = cityName.charAt(0).toUpperCase() + cityName.slice(1)
 
-    const URLCity = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=2&appid=${APIkey}&lang=es`
+    const URLCity = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${APIkey}&lang=es`
     const reqCity = await fetch(URLCity)
     const cityJson = await reqCity.json()
-    let dataCity = [cityJson[1].lat, cityJson[1].lon]
-    // console.log(cityJson)
+    let dataCity = [cityJson[0].lat, cityJson[0].lon]
+    console.log(cityJson)
     // console.log(cityJson[1].country)
     // console.log(dataCity)
 
@@ -22,6 +22,7 @@ async function fetchCity(){
     const URLApi = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}&lang=es`
     const reqWeather = await fetch(URLApi)
     const weatherJson = await reqWeather.json()
+    console.log(weatherJson)
     // console.log(weatherJson)
 
     //presentar informacion en pantalla
@@ -31,18 +32,22 @@ async function fetchCity(){
     const imgWheater = document.createElement('img')
     const nCity = document.createElement('h3')
     const temp = document.createElement('p')
+    const descriptionValue = document.createElement('p')
 
     container.className = 'cardWeather'
     imgWheater.className = 'imgWeather'
     nCity.className = 'nameCity'
     temp.className = 'weatherValue'
+    descriptionValue.className = 'countryName'
+
 
     nCity.textContent = `Ciudad: ${weatherJson.name}`
     imgWheater.src = `http://openweathermap.org/img/wn/${weatherJson.weather[0].icon}@2x.png`
     temp.textContent = `temperatura: ${parseInt(weatherJson.main.temp - 273.15)}° C`
+    descriptionValue.textContent = `${weatherJson.weather[0].description}`
 
     main.replaceChildren('')
-    container.append(imgWheater, nCity, temp)
+    container.append(imgWheater, nCity, temp, descriptionValue)
     main.appendChild(container)
     // container.textContent= 'Hola'
     
